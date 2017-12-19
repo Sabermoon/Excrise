67a4e15776863daec59a61d165bc6179b0d9b32f
Select <001>
2017-12-19 15:28:26 +0800

    first

---
+++ Excrise/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Excrise
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "选定的目录：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "预览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(36, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(426, 341);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "原文件名";
+            this.columnHeader1.Width = 118;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "新文件名";
+            this.columnHeader2.Width = 118;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "文件后缀";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "状态";
+            this.columnHeader4.Width = 117;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(517, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 69);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "开始";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "从第";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(518, 61);
+            this.textBox2.MaxLength = 50;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 21);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(554, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "个字符开始删除";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(485, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 21);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "个字符";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 435);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "批量文件名修改 V1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Excrise/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Excrise {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private FolderBrowserDialog contents = null;                //指定数据成员
+        private class CheckOut {
+            public string name;
+            public long NUM = 1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {        //目录选定
+            contents = new FolderBrowserDialog();
+            if (contents.ShowDialog() == DialogResult.OK) {
+                //label1.Text = @"选择的目录为：" + contents.SelectedPath;
+                textBox1.Text = contents.SelectedPath;
+            }contents.SelectedPath = textBox1.Text;
+            if (!Directory.Exists(contents.SelectedPath)) {
+                MessageBox.Show(@"目录无效！");
+                return;
+            }DirectoryInfo folder = new DirectoryInfo(contents.SelectedPath);
+            listView1.Items.Clear();
+            FileInfo[] info = folder.GetFiles();
+            foreach (FileInfo item in info)
+                if (item.Extension != "")
+                    listView1.Items.Add(new ListViewItem(new string[]
+                        {item.Name.Replace(item.Extension, ""), null, item.Extension, null}));
+                else
+                    listView1.Items.Add(new ListViewItem(new string[] {item.Name, null, null, null}));
+        }
+
+        private void Button2_Click(object sender, EventArgs e) {                 //完成越界审查
+            int begin = Convert.ToInt32(textBox2.Text) - 1;
+            int num = Convert.ToInt32(textBox3.Text);
+            if (begin < 0) begin = 0;
+            long number = 1;
+            foreach (ListViewItem item in listView1.Items) {
+                if (begin + num > item.SubItems[0].Text.Length)
+                    if (begin <= item.SubItems[0].Text.Length)
+                        item.SubItems[1].Text = item.SubItems[0].Text.Remove(begin);
+                    else {
+                        item.SubItems[1].Text = item.SubItems[0].Text;
+                        item.SubItems[3].Text = @"超出索引范围";
+                    }
+                else item.SubItems[1].Text = item.SubItems[0].Text.Remove(begin, num);
+                List<CheckOut> check = new List<CheckOut>();                     //完成重名修正
+                if (File.Exists(contents.SelectedPath + "\\" + item.SubItems[1].Text + item.SubItems[2].Text)) {
+                    item.SubItems[3].Text = @"文件名重复，已修改";
+                    do {
+                        check.Add(new CheckOut { name = item.SubItems[0].Text });
+                        check[0].NUM++;
+                        item.SubItems[1].Text = item.SubItems[0].Text + @"(" +   @")";
+                    } while (File.Exists(contents.SelectedPath + "\\" + item.SubItems[1].Text + item.SubItems[2].Text));
+                }listView1.Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            foreach (ListViewItem item in listView1.Items){
+                FileInfo temp =
+                    new FileInfo(contents.SelectedPath + "\\" + item.SubItems[0].Text + item.SubItems[2].Text);
+                temp.MoveTo(contents.SelectedPath+"\\"+ item.SubItems[1].Text+item.SubItems[2].Text);
+                if (item.SubItems[3].Text != null)
+                    item.SubItems[3].Text = @"重命名完成";
+            }
+        }
+    }
+}
